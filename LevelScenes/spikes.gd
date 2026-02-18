f252467595b5d4dbdbde9901cf26feaf8338b04c
Pablitodmd-Dev <pablodelgado03@gmail.com>
2026-02-18 14:33:02 +0100

    Fixed Level 2 puzzle And Level 3 Done

---
+++ LevelScenes/spikes.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@onready var death_sound = $"../DeathSound" 
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		var music = body.get_node_or_null("MusicNode")
+		if music:
+			music.stop()
+		
+		if death_sound:
+			death_sound.play()
+
+		body.set_physics_process(false)
+
+		if body.has_node("AnimatedSprite2D"):
+			body.get_node("AnimatedSprite2D").stop() 
+		
+		await get_tree().create_timer(5.0).timeout
+		get_tree().change_scene_to_file("res://level_select.tscn")
