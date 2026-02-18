a4cb76221e46438a966012c66ee68c72cbbf8198
Pablitodmd-Dev <pablodelgado03@gmail.com>
2026-02-18 09:17:01 +0100

    First Level Terminated And Creating Gems

--- GameManager.gd
+++ GameManager.gd
@@ -5,4 +5,4 @@ var niveles_desbloqueados = 1 # Empezamos con el 1
 func ganar_nivel(numero_actual):
 	if numero_actual == niveles_desbloqueados:
 		niveles_desbloqueados += 1
-	get_tree().change_scene_to_file("res://LevelSelectorScreen.tscn")
+	get_tree().change_scene_to_file("res://level_select.tscn")
