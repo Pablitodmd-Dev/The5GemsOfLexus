f5e759ac386e22994e33a642c105f5a70b23feb7
Pablitodmd-Dev <pablodelgado03@gmail.com>
2026-02-19 16:49:21 +0100

    Pause Menu in All Levels and Level 6 Fixed Bug

--- LevelScenes/FinalGem.gd
+++ LevelScenes/FinalGem.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var win_sound = $AudioStreamPlayer2D
+@export var current_level_number = 6
 
 func _on_body_entered(body):
 	if body.name == "Player":
@@ -8,10 +9,10 @@ func _on_body_entered(body):
 			win_sound.play()
 
 		body.set_physics_process(false)
-
+		
 		if "velocity" in body:
 			body.velocity = Vector2.ZERO
-
+		
 		await get_tree().create_timer(10.0).timeout
-
 		get_tree().change_scene_to_file("res://level_select.tscn")
+		GameManager.win_level(current_level_number)
